ed0e17c53e1044a7b92abeb739be3d0bb643d237
thomas <thomas@Thomas-VirtualBox>
2021-05-09 05:00:55 -0700

    New endpoint

--- src/main/java/ca/dumezthomas/toptalproject/server/api/UserAPI.java
+++ src/main/java/ca/dumezthomas/toptalproject/server/api/UserAPI.java
@@ -15,6 +15,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -37,48 +38,39 @@ public class UserAPI
 {
 	@EJB(beanName = "UserEJB")
 	private DAOLocal<User> userDAO;
-	
+
 	@Context
 	SecurityContext securityContext;
 
 	@GET
-	@Secured(Role.ADMIN)
+	@Secured({ Role.USER, Role.ADMIN })
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response getAll()
+	public Response getAll(@QueryParam("username") String username)
 	{
-		try
-		{
-			List<User> userList = userDAO.readAll();
-			userList.stream().forEach(u -> u.setPassword("******"));
-			Collections.sort(userList);
-			
-			String jsonString = new Gson().toJson(userList);
-			return Response.ok().entity(jsonString).build();
-		}
-		catch (Exception e)
-		{
-			return Response.serverError().entity("Read users failed: " + e.getMessage()).build();
-		}
+		if (username != null)
+			return getUserByUsername(username);
+		else
+			return getUsers();
 	}
 
 	@GET
-	@Secured({Role.USER, Role.ADMIN})
+	@Secured({ Role.USER, Role.ADMIN })
 	@Path("{id}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response get(@PathParam("id") Long id)
 	{
 		try
 		{
-			if(securityContext.isUserInRole(Role.USER))
+			if (securityContext.isUserInRole(Role.USER))
 			{
 				Principal principal = securityContext.getUserPrincipal();
-				if(Long.valueOf(principal.getName()) != id)
+				if (Long.valueOf(principal.getName()) != id)
 					return Response.status(Status.FORBIDDEN).entity("Not authorized").build();
 			}
-			
+
 			User user = userDAO.read(id);
 			user.setPassword("******");
-			
+
 			String jsonString = new Gson().toJson(user);
 			return Response.ok().entity(jsonString).build();
 		}
@@ -89,7 +81,7 @@ public class UserAPI
 	}
 
 	@POST
-	@Secured({Role.USER, Role.ADMIN})
+	@Secured({ Role.USER, Role.ADMIN })
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response add(User user)
@@ -98,11 +90,11 @@ public class UserAPI
 		{
 			String hash = Authentication.hashPassword(user.getPassword());
 			user.setPassword(hash);
-			
+
 			Set<Role> role = new HashSet<>();
 			role.add(new Role(Role.USER));
 			user.setRole(role);
-			
+
 			Long id = userDAO.create(user);
 			String jsonString = new Gson().toJson(id);
 			return Response.status(Status.CREATED).entity(jsonString).build();
@@ -114,7 +106,7 @@ public class UserAPI
 	}
 
 	@PATCH
-	@Secured({Role.USER, Role.ADMIN})
+	@Secured({ Role.USER, Role.ADMIN })
 	@Path("{id}")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
@@ -122,13 +114,13 @@ public class UserAPI
 	{
 		try
 		{
-			if(securityContext.isUserInRole(Role.USER))
+			if (securityContext.isUserInRole(Role.USER))
 			{
 				Principal principal = securityContext.getUserPrincipal();
-				if(Long.valueOf(principal.getName()) != id)
+				if (Long.valueOf(principal.getName()) != id)
 					return Response.status(Status.FORBIDDEN).entity("Not authorized").build();
 			}
-			
+
 			userDAO.updateStrings(id, userIdentity.getFirstName(), userIdentity.getLastName());
 			return Response.ok().entity("{}").build();
 		}
@@ -137,9 +129,9 @@ public class UserAPI
 			return Response.serverError().entity("Update user failed: " + e.getMessage()).build();
 		}
 	}
-	
+
 	@PATCH
-	@Secured({Role.USER, Role.ADMIN})
+	@Secured({ Role.USER, Role.ADMIN })
 	@Path("{id}/password")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
@@ -147,24 +139,24 @@ public class UserAPI
 	{
 		try
 		{
-			if(securityContext.isUserInRole(Role.USER))
+			if (securityContext.isUserInRole(Role.USER))
 			{
 				Principal principal = securityContext.getUserPrincipal();
-				if(Long.valueOf(principal.getName()) != id)
+				if (Long.valueOf(principal.getName()) != id)
 					return Response.status(Status.FORBIDDEN).entity("Not authorized").build();
 			}
-			
-			if(!passwords.getNewPassword1().equals(passwords.getNewPassword2()))
+
+			if (!passwords.getNewPassword1().equals(passwords.getNewPassword2()))
 				throw new Exception("Different passwords");
-			
+
 			User user = userDAO.read(id);
-			
-			if(!Authentication.isSamePassword(passwords.getOldPassword(), user.getPassword()))
+
+			if (!Authentication.isSamePassword(passwords.getOldPassword(), user.getPassword()))
 				throw new Exception("Invalid password");
-			
+
 			String hash = Authentication.hashPassword(passwords.getNewPassword1());
 			userDAO.updateString(id, hash);
-			
+
 			return Response.ok().entity("{}").build();
 		}
 		catch (Exception e)
@@ -189,4 +181,48 @@ public class UserAPI
 			return Response.serverError().entity("Delete user failed: " + e.getMessage()).build();
 		}
 	}
+	
+	private Response getUsers()
+	{
+		try
+		{
+			if (!securityContext.isUserInRole(Role.ADMIN))
+				return Response.status(Status.FORBIDDEN).entity("Not authorized : User not in role").build();
+
+			List<User> userList = userDAO.readAll();
+			userList.stream().forEach(u -> u.setPassword("******"));
+			Collections.sort(userList);
+
+			String jsonString = new Gson().toJson(userList);
+			return Response.ok().entity(jsonString).build();
+		}
+		catch (Exception e)
+		{
+			return Response.serverError().entity("Read users failed: " + e.getMessage()).build();
+		}
+	}
+
+	private Response getUserByUsername(String username)
+	{
+		try
+		{
+			User user = userDAO.readByStringId(username);
+			user.setPassword("******");
+
+			if (securityContext.isUserInRole(Role.USER))
+			{
+				Principal principal = securityContext.getUserPrincipal();
+				if (Long.valueOf(principal.getName()) != user.getId())
+					return Response.status(Status.FORBIDDEN).entity("Not authorized").build();
+			}
+
+			String jsonString = new Gson().toJson(user);
+			return Response.ok().entity(jsonString).build();
+		}
+		catch (Exception e)
+		{
+			return Response.serverError().entity("Read user failed: " + e.getMessage()).build();
+		}
+	}
+
 }
